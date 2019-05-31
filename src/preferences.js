497ae351a2f6addba93d6a5d7e0aef0df3cc2657
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-05-31 21:02:09 +0800

    Graduate Seatsmart Remote from experimental

--- src/preferences.js
+++ src/preferences.js
@@ -43,7 +43,8 @@ export default {
 									Weight: 'medium'
 								}
 							],
-							behaviorToTally: '(-) Absent'
+							behaviorToTally: '(-) Absent',
+							calculationInterval: 'weekly'
 						}
 
 						this.setPreferences(obj)
