b96a254c53f8bfe549014b3410630180b3a8d8f7
Thaddeus McCleary <thaddeusmccleary@Thaddeuss-MacBook.local>
2019-08-24 19:43:40 +0800

    Fix onboarding documentation display logic

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -31,7 +31,170 @@
 			</p>
 		</section>
 		<!-- Show help about chart UI -->
-		<section v-if="progress.indexOf('created class') !== -1 && progress.indexOf('explored remote features') !== -1">
+		<section v-if="progress.length == 2">
+			<h4>
+				Using a Chart
+			</h4>
+			<p>
+				A class chart contains rows and columns of Student Cards representing
+				seating in the classroom.  Chart Tools at the bottom of the screen provide
+				other options.
+			</p>
+			<br>
+			<Tabs :sections="[{label: 'Student Cards', color: 'gray'},  {label: 'Chart Tools', color: 'yellow'}]">
+				<template slot="Student Cards">
+					<p class="center">{{ caption }}</p>
+					<div class="demo-area">
+						<NameCard 
+							class="demo-card" 
+							type="demo" 
+							v-on:show-caption="showCaption"
+						/>
+					</div>
+				</template>
+				<template slot="Chart Tools">
+					<ul>
+						<li>
+							<img src="@/assets/editwhite.svg" alt="edit icon">
+							<h5>Edit Mode</h5>
+							<p>
+								You can can add, edit, or delete students directly from the chart.  If you want to
+								edit class information (name, rows, columns, etc.), you can click the pencil icon
+								that appears on the right side of the class name.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/rearrange.svg" alt="rearrange icon">
+							<h5>Rearrange Student Seating</h5>
+							<p>
+								You can return to this screen any time to change the seating of one or more students.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/highlight.svg" alt="highlight icon">
+							<h5>Highlight Students</h5>
+							<p>
+								Drag and drop a color from 15 options to add another way to distinguish students.
+								This is especially useful when organizing teams.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/random.svg" alt="select random student icon">
+							<h5>**Random Student Selector</h5>
+							<p>
+								Well...randomish.  Students are shuffled to prevent a student from being chosen
+								consecutively.  Click the button again to select another student.  Double-click to
+								make the selection disappear.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/changecard.svg" alt="change card style icon">
+							<h5>Toggle Student Card Styles</h5>
+							<p>
+								By default, student cards show indicators about absences and notes.  You can click
+								this button to switch back and forth to a more simple card style that hides these
+								records.  This is useful if other students are looking at the screen.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/trendicon.svg" alt="show participation trends icon">
+							<h5>Recent Participation Trends</h5>
+							<p>
+								Clicking this button displays each student's recent participation trend.
+								This is helpful when updating participation grades in Blackboard.  You can change the interval being considered in this calculation in the <button class="inner-link" @click="$emit('change-content', 'cards')">Name Cards</button> section settings.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/present.svg" alt="present icon">
+							<h5>Chart Presentation Mode</h5>
+							<p>
+								When you would like to show the seating chart to students over a projector, click this
+								button to flip the chart orientation and hide participation indicators.  It also hides
+								the chart tools to expand screen space.  Click again to disable.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/groupnote.svg" alt="group note icon">
+							<h5>Add a Bulk Note</h5>
+							<p>
+								Apply one participation note to multiple students.  This is helpful, for instance, when
+								wanting to quickly note which students are late when the class bell rings.
+							</p>
+						</li>
+						<li>
+							<img src="@/assets/star-deselect.svg" alt="deselect students icon">
+							<h5>Deselect All Students</h5>
+							<p>
+								Get rid of all of the stars in one click!  That sounds a little depressing, but 
+								it really is convenient sometimes.
+							</p>
+						</li>
+					</ul>
+				</template>
+			</Tabs>
+		</section>
+		<!-- Feature discovery for Remote and Activities -->
+		<section v-if="progress.length > 2 && progress.length < 6">
+			<h4>
+				Connecting Remote Devices
+			</h4>
+			<p>
+				Seatsmart is and always will be an offline-first application.  All features essential to managing classroom participation records do not require an Internet connection and all data is stored on your computer (check for the "Seatsmart" folder in your computer's My Documents / Documents area).
+			</p>
+			<p>
+				Features such as Remote and Activities are completely optional and are only meant to add convenience or opportunities for students to participate.
+			</p>
+			<br><br>
+			<h4>
+				Remote
+			</h4>
+			<p>
+				When viewing a class chart, you may have noticed the <img class="embedded-icon" src="@/assets/remote.svg" alt="remote icon"> icon at the upper right corner of the screen.  Clicking this icon (if you are connected to the Internet) will reveal a QR code that you can scan with your mobile device.  This will set up a secure to allow you control basic features of Seatsmart remotely.
+			</p>
+			<p><i>Why would you use this?</i></p>
+			<p>
+				Using Seatsmart actively in class would otherwise require you to be near your computer (at the front of the room) and to switch between other programs you may be running (Powerpoint).  Using a mobile device as a remote instead allows you to move around the classroom, interacting with students and recording participation notes as they occur without needing to go back to the computer.
+			</p>
+			<div class="personal-note">
+				<img class="profile-image" src="@/assets/thaddeus.png" alt="sketched image of Thaddeus">
+				<div class="text-bubble">
+					<p>
+						My favorite way to use the Remote is to stand toward the back of the classroom with the Seatsmart chart in presentation mode, projected at the front of the classroom.  I use my iPad to have Seatsmart choose a random student <img class="embedded-icon-inverted" src="@/assets/randomblack.svg" alt="dice icon">, which the students can see illuminated in yellow on the screen in front of them.
+					</p>
+					<p>
+						Then, I may ask a pop quiz question or for an example that contributes to the lesson.  Then, I tap the <img class="embedded-icon-inverted" src="@/assets/note.svg" alt="note icon">  button to give the student participation points before moving on.
+					</p>
+				</div>
+			</div>
+			<h4>
+				Connecting with Students' Devices (Activities)
+			</h4>
+			<p>
+				You can find the Activities icon (<img class="embedded-icon-plain" src="@/assets/activities-circle.svg" alt="activities icon">) on the upper-left corner of the screen both when you first open Seatsmart and when you are viewing a class chart.  When clicking this icon from the starting screen, you can create new realtime activities for your classes in advance and manage existing ones.  From a class chart, this icon acts as a launch window to help you initiate the activity during class.
+			</p>
+			<p>
+				Launching an activity allows students to securely connect their devices with your computer via a QR code for a short but engaging event (such as a survey).  After you end the activity session, you will be prompted to choose whether or not to immediately add participation records for all active participants.
+			</p>
+			<div class="personal-note">
+				<img class="profile-image" src="@/assets/thaddeus.png" alt="sketched image of Thaddeus">
+				<div class="text-bubble">
+					<p>
+						This is a very new feature in Seatsmart that I look forward to exploring in my classes this fall.  I hope you find it convenient that it integrates with your other classroom records and does not require logging into anything, which cannot be said of Kahoot, Socrative, or other popular education apps.
+					</p>
+					<p>
+						For example, before beginning a Composition class about ways to structure writing, I might launch a survey that asks students how long a typical paragraph should be.  The idea is not to look for a "correct answer", but instead to get a baseline of what students have heard/guessed before starting the lesson.  Students themselves may be interested in watching the screen as results are tabulated in realtime.
+					</p>
+					<p>
+						Then, I can quickly add participation points when prompted after I end the survey.  Behind the scenes, Seatsmart determines which students were "genuine" participants by ensuring that only students who selected an option during the time allowed receive credit.
+					</p>
+				</div>
+			</div>
+			<p>
+				Tip: Take advantage of the preview feature when creating an activity to get a sense of a student's experience.  Activities should feel more like a game than a quiz.  Also, keep in mind language barriers (keep wording concise or define beforehand).
+			</p>
+		</section>
+		<!-- Final help docs / onboarding complete -->
+		<section v-if="progress.length == 6">
 			<h4>
 				Using a Chart
 			</h4>
@@ -159,65 +322,6 @@
 			</p>
 			<p>
 				This feature will be under active development throughout the 2019-2020 academic year.  Bug reports, feature suggestions, and other comments about Activities are appreciated (see the <button class="inner-link" @click="$emit('change-content', 'about')">About</button> section for contact links).
-			</p>
-		</section>
-		<section v-else-if="progress.indexOf('explored remote features') == -1">
-			<h4>
-				Connecting Remote Devices
-			</h4>
-			<p>
-				Seatsmart is and always will be an offline-first application.  All features essential to managing classroom participation records do not require an Internet connection and all data is stored on your computer (check for the "Seatsmart" folder in your computer's My Documents / Documents area).
-			</p>
-			<p>
-				Features such as Remote and Activities are completely optional and are only meant to add convenience or opportunities for students to participate.
-			</p>
-			<br><br>
-			<h4>
-				Remote
-			</h4>
-			<p>
-				When viewing a class chart, you may have noticed the <img class="embedded-icon" src="@/assets/remote.svg" alt="remote icon"> icon at the upper right corner of the screen.  Clicking this icon (if you are connected to the Internet) will reveal a QR code that you can scan with your mobile device.  This will set up a secure to allow you control basic features of Seatsmart remotely.
-			</p>
-			<p><i>Why would you use this?</i></p>
-			<p>
-				Using Seatsmart actively in class would otherwise require you to be near your computer (at the front of the room) and to switch between other programs you may be running (Powerpoint).  Using a mobile device as a remote instead allows you to move around the classroom, interacting with students and recording participation notes as they occur without needing to go back to the computer.
-			</p>
-			<div class="personal-note">
-				<img class="profile-image" src="@/assets/thaddeus.png" alt="sketched image of Thaddeus">
-				<div class="text-bubble">
-					<p>
-						My favorite way to use the Remote is to stand toward the back of the classroom with the Seatsmart chart in presentation mode, projected at the front of the classroom.  I use my iPad to have Seatsmart choose a random student <img class="embedded-icon-inverted" src="@/assets/randomblack.svg" alt="dice icon">, which the students can see illuminated in yellow on the screen in front of them.
-					</p>
-					<p>
-						Then, I may ask a pop quiz question or for an example that contributes to the lesson.  Then, I tap the <img class="embedded-icon-inverted" src="@/assets/note.svg" alt="note icon">  button to give the student participation points before moving on.
-					</p>
-				</div>
-			</div>
-			<h4>
-				Connecting with Students' Devices (Activities)
-			</h4>
-			<p>
-				You can find the Activities icon (<img class="embedded-icon-plain" src="@/assets/activities-circle.svg" alt="activities icon">) on the upper-left corner of the screen both when you first open Seatsmart and when you are viewing a class chart.  When clicking this icon from the starting screen, you can create new realtime activities for your classes in advance and manage existing ones.  From a class chart, this icon acts as a launch window to help you initiate the activity during class.
-			</p>
-			<p>
-				Launching an activity allows students to securely connect their devices with your computer via a QR code for a short but engaging event (such as a survey).  After you end the activity session, you will be prompted to choose whether or not to immediately add participation records for all active participants.
-			</p>
-			<div class="personal-note">
-				<img class="profile-image" src="@/assets/thaddeus.png" alt="sketched image of Thaddeus">
-				<div class="text-bubble">
-					<p>
-						This is a very new feature in Seatsmart that I look forward to exploring in my classes this fall.  I hope you find it convenient that it integrates with your other classroom records and does not require logging into anything, which cannot be said of Kahoot, Socrative, or other popular education apps.
-					</p>
-					<p>
-						For example, before beginning a Composition class about ways to structure writing, I might launch a survey that asks students how long a typical paragraph should be.  The idea is not to look for a "correct answer", but instead to get a baseline of what students have heard/guessed before starting the lesson.  Students themselves may be interested in watching the screen as results are tabulated in realtime.
-					</p>
-					<p>
-						Then, I can quickly add participation points when prompted after I end the survey.  Behind the scenes, Seatsmart determines which students were "genuine" participants by ensuring that only students who selected an option during the time allowed receive credit.
-					</p>
-				</div>
-			</div>
-			<p>
-				Tip: Take advantage of the preview feature when creating an activity to get a sense of a student's experience.  Activities should feel more like a game than a quiz.  Also, keep in mind language barriers (keep wording concise or define beforehand).
 			</p>
 		</section>
 	</div>
