f83e670b0e8892ef63e9232fab9d716baaaf9c84
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2019-11-18 10:14:38 +0800

    Remove unnecessary punctuation

--- src/components/ActivityAdapter.vue
+++ src/components/ActivityAdapter.vue
@@ -44,7 +44,7 @@
 						</div>
 					</section>
 					<section class="button-container" v-if="activityStage == 'launched'">
-						<h3 v-if="roomID !== ''">Scan to Connect:</h3>
+						<h3 v-if="roomID !== ''">Scan to Connect</h3>
 						<h3 v-else>Connecting...</h3>
 						<qriously 
 							v-if="roomID !== ''"
