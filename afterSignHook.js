28ad477a9a48c2c18f1187a10ff9e4c2544d5212
Thaddeus McCleary <thaddeusmccleary@icloud.com>
2020-07-07 17:59:21 -0500

    Test notarization settings

---
+++ afterSignHook.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+var electron_notarize = require('electron-notarize');
+
+module.exports = async function (params) {
+    // Only notarize the app on Mac OS only.
+    if (process.platform !== 'darwin') {
+        console.log('not mac')
+        return;
+    }
+    console.log('afterSign hook triggered', params);
+
+    // Same appId in electron-builder.
+    let appId = 'com.thaddeusmccleary.seatsmart';
+
+    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+    if (!fs.existsSync(appPath)) {
+        throw new Error(`Cannot find application at: ${appPath}`);
+    }
+
+    console.log(`Notarizing ${appId} found at ${appPath}`);
+
+    try {
+        await electron_notarize.notarize({
+            appBundleId: appId,
+            appPath: appPath,
+            appleId: process.env.ID,
+            appleIdPassword: process.env.PASSWORD,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(`Done notarizing ${appId}`);
+};
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      "appId": "com.thaddeusmccleary.seatsmart",
+      "afterSign": "./afterSignHook.js",
+      "mac": {
+	      "entitlements": "./build/entitlements.mac.inherit.plist"
+	   }
     }
   }
 }
